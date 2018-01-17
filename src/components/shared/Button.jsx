8aa03dc0d3c44bc68189899995173bdfea280cd4
Rener Baffa da Silva <renerbaffa@gmail.com>
2018-01-17 18:35:54 -0200

    Reactoring of button style

--- src/components/shared/Button.jsx
+++ src/components/shared/Button.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+
+import './Button.css';
 
 /*
   Button component is an adapter.
   Imagine that in the future we move to react-bootstrap or material-ui
   In this case we only need to replase default button by the library button
 */
-const Button = ({ children, ...other }) => (
-  <button {...other}>
+const Button = ({ children, className, ...other }) => (
+  <button {...other} className={cx(className, 'Button')}>
     {children}
   </button>
 );
+
+Button.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
+};
+
+Button.defaultProps = {
+  children: null,
+  className: '',
+};
 
 export default Button;
