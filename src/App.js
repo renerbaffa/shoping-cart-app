61dff5638e209c16dfa8727b64aeb4fa2ba85db1
Rener Baffa da Silva <renerbaffa@gmail.com>
2018-01-17 18:22:13 -0200

    Refactoring: better positioning global styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 
 import Menu from './components/template/Menu';
+import ProductsPage from './pages/ProductsPage';
+
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Menu />
+        <ProductsPage />
       </div>
     );
   }
