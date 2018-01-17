0986b73fbaaa59136782dbe41975dc1691008267
Rener Baffa da Silva <renerbaffa@gmail.com>
2018-01-17 18:58:36 -0200

    Refactor: Input Unit Test update

--- src/components/shared/Input.spec.js
+++ src/components/shared/Input.spec.js
@@ -3,9 +3,11 @@ import renderer from 'react-test-renderer';
 
 import Input from './Input';
 
-it('renders correctly', () => {
-  const tree = renderer
-    .create(<Input className="className" value="value" placeholder="Search" value="Search" />)
-    .toJSON();
-  expect(tree).toMatchSnapshot();
+describe('<Input />', () => {
+  it('should render correctly', () => {
+    const tree = renderer
+      .create(<Input className="className" value="value" placeholder="Search" value="Search" />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
