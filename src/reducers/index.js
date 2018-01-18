a18a9f8810535214d7dae1e4ce566388306c5019
Rener Baffa da Silva <renerbaffa@gmail.com>
2018-01-18 11:49:41 -0200

    adding communication on redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 
+import communication from './communication';
 import products from './products';
 
 const appReducer = combineReducers({
+  communication,
   products,
 });
 
