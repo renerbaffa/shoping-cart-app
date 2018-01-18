9bac0de60d8c22202df77b438ddcdc92239c4669
Rener Baffa da Silva <renerbaffa@gmail.com>
2018-01-18 20:12:46 -0200

    Creating products mock

---
+++ src/mocks/Products.js
@@ -0,0 +1,20 @@
+const PRODUCTS = [
+  {
+    description: 'description 1',
+    image: 'http://lorempixel.com/400/200/technics/',
+    name: 'Chef Anton Cajun Seasoning',
+    productID: 4,
+    unitPrice: 22,
+    unitsInStock: 53,
+  },
+  {
+    description: 'description 2',
+    image: 'http://lorempixel.com/400/200/technics/',
+    name: '"Guaraná Fantástica"',
+    productID: 24,
+    unitPrice: 4.5,
+    unitsInStock: 20,
+  },
+];
+
+export default PRODUCTS;
