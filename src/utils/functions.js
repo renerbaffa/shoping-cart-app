82e7556b7a4b23907bb22473e7a43c009945f6b2
Rener Baffa da Silva <renerbaffa@gmail.com>
2018-01-19 10:18:47 -0200

    Creating and unit testing filter function

---
+++ src/utils/functions.js
@@ -0,0 +1,25 @@
+export const INITIAL_STATE = {
+  ids: [],
+  content: {},
+};
+
+export function filterByText(data = INITIAL_STATE, text = '') {
+  const ids = [];
+  let content = {};
+
+  data.ids.forEach(id => {
+    const element = data.content[id];
+    if (element.name.indexOf(text) >= 0) {
+      ids.push(id);
+      content = {
+        ...content,
+        [id]: element,
+      };
+    }
+  });
+
+  return {
+    ids,
+    content,
+  };
+}
---
+++ src/utils/functions.test.js
@@ -0,0 +1,43 @@
+import { filterByText, INITIAL_STATE } from './functions';
+
+import PROJECTS from '../mocks/Products';
+import convertToIdsAndContent from '../normalizers/productsNormalize';
+
+const NORMALIZED_PROJECTS = convertToIdsAndContent(PROJECTS);
+const FILTER = 'Fant';
+
+describe('filterByField', () => {
+  let filtered;
+
+  describe('given no data', () => {
+    beforeEach(() => {
+      filtered = filterByText();
+    });
+
+    it('should return INITIAL_STATE', () => {
+      expect(filtered).toEqual(INITIAL_STATE);
+    });
+  });
+
+  describe('providing data', () => {
+    describe('given no filter text', () => {
+      beforeEach(() => {
+        filtered = filterByText(NORMALIZED_PROJECTS);
+      });
+
+      it('should return all data ', () => {
+        expect(filtered).toEqual(NORMALIZED_PROJECTS);
+      });
+    });
+
+    describe('providing filter text', () => {
+      beforeEach(() => {
+        filtered = filterByText(NORMALIZED_PROJECTS, FILTER);
+      });
+
+      it('should return filtered data', () => {
+        expect(filtered.ids).toHaveLength(1);
+      });
+    });
+  });
+});
